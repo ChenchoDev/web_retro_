2a397995d5b062542be3bfb3331a85cfd65faff2
ChenchoDev <chenchomarin@gmail.com>
2022-01-17 01:04:51 +0100

    Ultimos detalles v5

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,45 +1,55 @@
 <template>
   <div class="content_footer">
-    <div class="card-footer">
-      <ul>
-        <li class="txt1">CONTACTO</li>
-        <hr class="line">
-        <li class="txt">&copy; Fulgencio Marín Talavera</li>
-        <li class="txt"><a href="mailto:"> pedidos@weeteighty.com</a></li>
-        <li class="txt">Murcia - España</li>
-      </ul>
-    </div>
-    <div class="card-footer">
+    <div class="top_footer">
+      <div class="card-footer">
         <ul>
-        <li class="txt1">QUIENES SOMOS</li>
-        <hr class="line">
-        <li class="txt"><a href="#">Sobre nosotros</a></li>
-        <li class="txt"><a href="#">Contáctanos</a></li>
-        <li class="txt"><a href="#">Términos y condiciones</a></li>
-      </ul>
-    </div>
-    <div class="card-footer">
-          <ul>
-        <li class="txt1">AYUDA</li>
-        <hr class="line">
-        <li class="txt"><a href="#">F.A.Q</a></li>
-        <li class="txt"><a href="#">Envíos y entregas</a></li>
-        <li class="txt"><a href="#">Politica de devoluciones</a></li>
-      </ul>
-    </div>
-    <div class="card-footer">
-      <div class="credit_cards">
-        <div><i class=" fab fa-cc-visa"></i></div>
-        <div><i class=" fab fa-cc-mastercard"></i></div>
-        <div><i class=" fab fa-cc-paypal"></i></div>
-        <div><i class=" fab fa-cc-amazon-pay"></i></div>
+          <li class="txt1">CONTACTO</li>
+          <hr class="line" />
+          <li class="txt">&copy; Fulgencio Marín Talavera</li>
+          <li class="txt"><a href="mailto:"> pedidos@weeteighty.com</a></li>
+          <li class="txt">Murcia - España</li>
+        </ul>
       </div>
-      <div class="credit_cards">
-        <div><a href="#"><i class="fab fa-facebook"></i></a></div>
-        <div><a href="#"><i class="fab fa-twitter-square"></i></a></div>
-        <div><a href="#"><i class="fab fa-instagram-square"></i></a></div>
-       
+      <div class="card-footer">
+        <ul>
+          <li class="txt1">QUIENES SOMOS</li>
+          <hr class="line" />
+          <li class="txt"><a href="#">Sobre nosotros</a></li>
+          <li class="txt"><a href="#">Contáctanos</a></li>
+          <li class="txt"><a href="#">Términos y condiciones</a></li>
+        </ul>
       </div>
+      <div class="card-footer">
+        <ul>
+          <li class="txt1">AYUDA</li>
+          <hr class="line" />
+          <li class="txt"><a href="#">F.A.Q</a></li>
+          <li class="txt"><a href="#">Envíos y entregas</a></li>
+          <li class="txt"><a href="#">Politica de devoluciones</a></li>
+        </ul>
+      </div>
+      <div class="card-footer">
+        <div class="credit_cards">
+          <div><i class="fab fa-cc-visa"></i></div>
+          <div><i class="fab fa-cc-mastercard"></i></div>
+          <div><i class="fab fa-cc-paypal"></i></div>
+          <div><i class="fab fa-cc-amazon-pay"></i></div>
+        </div>
+        <div class="credit_cards">
+          <div>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+          </div>
+          <div>
+            <a href="#"><i class="fab fa-twitter-square"></i></a>
+          </div>
+          <div>
+            <a href="#"><i class="fab fa-instagram-square"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom_footer">
+        <h5>&copy; Copyright - Todos los derechos reservados</h5>
     </div>
   </div>
 </template>
@@ -49,24 +59,40 @@ export default {};
 </script>
 
 <style>
-.txt1{
-    font-weight: 200;
+.bottom_footer{
+    display: flex;
+    flex-direction:row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    background-color:#a27838 ;
+    color:#761405;
+    height:20px;
+    font-weight:200;
+}
+.content_footer{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+}
+.txt1 {
+  font-weight: 200;
   font-size: 12px;
 }
 
-.credit_cards{
-    display: flex;
-    flex-direction: row;
-    flex-grow: 1;
-    justify-content:space-evenly;
-    align-items:center;
-    width: 100%;
-    
+.credit_cards {
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
 }
-.credit_cards i{
-    font-size:25px;
+.credit_cards i {
+  font-size: 25px;
 }
-.txt{
+.txt {
   font-weight: 200;
   font-size: 10px;
 }
@@ -76,7 +102,6 @@ export default {};
   align-items: center;
   color: #a27838;
   width: 25%;
-
 }
 .card-footer a {
   text-decoration: none;
@@ -89,7 +114,7 @@ export default {};
 .card-footer ul > li {
   list-style: none;
 }
-.content_footer {
+.top_footer {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
